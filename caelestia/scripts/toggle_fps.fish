a90c0d85f5406b5412678b7735ba26689e316b86
Ahhaha9119 <skyblocksians@gmail.com>
2025-09-23 10:58:53 +0800

    changing toggle_fps varriables

--- caelestia/scripts/toggle_fps.fish
+++ caelestia/scripts/toggle_fps.fish
@@ -5,28 +5,28 @@ set mango_config $HOME/.config/MangoHud/MangoHud.conf
 
 # Target refresh rates
 set rate_high 240
-set rate_low 85
+set rate_low 60
 
 # Target FPS limits
 set limit_high 237
-set limit_low 82
+set limit_low 60
 
-set common_fps_limits 60
+# set common_fps_limits 60
 
 # Extract current refresh rate from the monitor line and current FPS limit from MangoHud config
 set current_rate (grep -Po '(?<=@)\d+' $config_file)
-#set current_limit (grep -Po '(?<=fps_limit=)\d+' $mango_config)
+# set current_limit (grep -Po '(?<=fps_limit=)\d+' $mango_config)
 
 if test "$current_rate" = "$rate_high"
     set new_rate $rate_low
     set new_limit $limit_low
 
-    sed -i "s/^\(fps_limit=\)$limit_high,,/\1$common_fps_limits,$new_limit,/" $mango_config
+    sed -i "s/^\(fps_limit=\)$limit_high,,/\1$new_limit,,/" $mango_config
 else
     set new_rate $rate_high
     set new_limit $limit_high
 
-    sed -i "s/^\(fps_limit=\)$common_fps_limits,$limit_low,/\1$new_limit,,/" $mango_config
+    sed -i "s/^\(fps_limit=\)$limit_low,,/\1$new_limit,,/" $mango_config
 end
 
 # Replace only the refresh rate part
